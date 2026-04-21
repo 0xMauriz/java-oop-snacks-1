2fc34340e298e9ea57cddb796b5d42201b6a29f5
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2026-04-21 19:04:24 +0200

    Primo snack comple(tato)

---
+++ src/oop/snacks/Main.java
@@ -0,0 +1,13 @@
+package oop.snacks;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Studenti francoPaglia = new Studenti("Franco", "Paglia", 23);
+
+        System.out.println(francoPaglia.getAllInfo());
+
+    }
+
+}
---
+++ src/oop/snacks/Studenti.java
@@ -0,0 +1,21 @@
+package oop.snacks;
+
+public class Studenti {
+
+    private String nome;
+    private String cognome;
+    private int eta;
+
+    public Studenti(String nome, String cognome, int eta) {
+
+        this.nome = nome;
+        this.cognome = cognome;
+        this.eta = eta;
+
+    }
+
+    public String getAllInfo() {
+        return this.nome + " " + this.cognome + ", " + this.eta + " anni";
+    }
+
+}
